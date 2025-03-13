b176d01cdee0c7e44fcc40c3a27905c6f089e5dc
raimeiga <yamataifrs@gmail.com>
2025-03-14 02:03:59 +0900

    調整

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function (){ 
+
+ //ハンバーガーメニュー
+  $('.c-ham').click(function(){
+    $(this).toggleClass('active')
+    $('.l-header__nav').toggleClass('active')
+  })
+  $('.l-header__list a').click(function(){
+    $('.c-ham').removeClass('active')
+     $('.l-header__nav').removeClass('active')
+  })
+
+//スライダー
+   $(".js-slider").slick({
+    autoplay: true,  //自動再生
+    autoplaySpeed: 1000,    
+    // arrows: true,
+    slidesToShow: 3, // 3枚ずつ表示
+    slidesToScroll: 1, // 1枚ずつスクロール
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev.svg" alt="Previous"></button>',  
+    nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next.svg" alt="Next"></button>',
+
+
+  });
+
+});
